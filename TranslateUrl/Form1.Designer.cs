00b1659af87359a61f299d0fa0dedbf27c867395
nicor <chairman101222@gmail.com>
2019-02-15 22:42:25 +0800

    add Url Encoding&Decode

---
+++ TranslateUrl/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace TranslateUrl
+{
+    partial class mainFrom
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.cmbEncoding = new System.Windows.Forms.ComboBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(35, 38);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(747, 189);
+            this.txtInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutput.Location = new System.Drawing.Point(35, 281);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(747, 189);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncode.Location = new System.Drawing.Point(680, 487);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(102, 38);
+            this.btnEncode.TabIndex = 4;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // cmbEncoding
+            // 
+            this.cmbEncoding.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbEncoding.FormattingEnabled = true;
+            this.cmbEncoding.Location = new System.Drawing.Point(313, 496);
+            this.cmbEncoding.Name = "cmbEncoding";
+            this.cmbEncoding.Size = new System.Drawing.Size(153, 23);
+            this.cmbEncoding.TabIndex = 5;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDecode.Location = new System.Drawing.Point(526, 487);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(102, 38);
+            this.btnDecode.TabIndex = 6;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // mainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 549);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.cmbEncoding);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Name = "mainFrom";
+            this.Text = "TranslateUrl";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.ComboBox cmbEncoding;
+        private System.Windows.Forms.Button btnDecode;
+    }
+}
+
---
+++ TranslateUrl/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace TranslateUrl
+{
+    public partial class mainFrom : Form
+    {
+        public mainFrom()
+        {
+            InitializeComponent();
+
+            cmbEncoding.DataSource = Encoding.GetEncodings();
+            cmbEncoding.DisplayMember = "Name";
+            cmbEncoding.ValueMember = "Name";
+            cmbEncoding.SelectedValue = "utf-8";
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            if (cmbEncoding.SelectedValue == null)
+            {
+                MessageBox.Show("please select a Encoding type.", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            txtOutput.Text = HttpUtility.UrlEncode(txtInput.Text, System.Text.Encoding.GetEncoding(cmbEncoding.SelectedValue.ToString()));
+
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            if (cmbEncoding.SelectedValue == null)
+            {
+                MessageBox.Show("please select a Encoding type.", "info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            txtOutput.Text = HttpUtility.UrlDecode(txtInput.Text, System.Text.Encoding.GetEncoding(cmbEncoding.SelectedValue.ToString()));
+        }
+    }
+}
